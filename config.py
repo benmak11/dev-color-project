72a85ba571a7ee23b95a28cf025cb66d39bccb99
Ben Makusha <btmakusha@gmail.com>
2025-02-17 22:06:54 -0500

    Update and remove secrets

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
-OPENAI_API_KEY = "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"
-# OpenAI embedding model
-EMBEDDING_MODEL = "text-embedding-ada-002" 
-# OpenAI language model (Might have to use "gpt-3.5-turbo" if "gpt-4" is not available)
-GPT_MODEL = "gpt-4o" 
+OPENAI_API_KEY = "your_openai_api_key_here"
+
+EMBEDDING_MODEL = "text-embedding-ada-002" # OpenAI embedding model
+
+GPT_MODEL = "gpt-4o" # OpenAI language model (Might have to use "gpt-3.5-turbo" if "gpt-4" is not available)
